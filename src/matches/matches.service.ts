d5b421a9e32840e99f491c25cf1b8b8862c660fe
Natorden <71587437+Natorden@users.noreply.github.com>
2022-04-04 15:14:22 +0200

    Application now has functioning repository with CRUD
    Application now works

--- src/matches/matches.service.ts
+++ src/matches/matches.service.ts
@@ -1,17 +1,66 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
 import { CreateMatchDto } from './dto/create-match.dto';
 import { UpdateMatchDto } from './dto/update-match.dto';
 import { Model } from 'mongoose';
 import { Match } from './entities/match.entity';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class MatchesService {
   constructor(
     @Inject('MATCH_MODEL') private readonly matchModel: Model<Match>,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
   ) {}
 
-  create(createMatchDto: CreateMatchDto) {
-    return 'This action adds a new match';
+  updateMatchInCache(value: Match) {
+    this.cacheManager.set<Match>(value.userUUID, value, {
+      ttl: 600,
+    });
+  }
+
+  async create(createMatchDto: CreateMatchDto): Promise<Match> {
+    const match = await this.matchModel
+      .findOne<Match>({
+        userUUID: createMatchDto.userMatchSenderUUID,
+      })
+      .exec();
+
+    if (match) {
+      if (createMatchDto.isAMatch) {
+        return this.matchModel
+          .findByIdAndUpdate(
+            {
+              useerUUID: createMatchDto.userMatchSenderUUID,
+            },
+            { $push: { likes: createMatchDto.userMatchReceiverUUID } },
+          )
+          .exec();
+      } else {
+        return this.matchModel
+          .findByIdAndUpdate(
+            {
+              useerUUID: createMatchDto.userMatchSenderUUID,
+            },
+            { $push: { dislikes: createMatchDto.userMatchReceiverUUID } },
+          )
+          .exec();
+      }
+    } else {
+      const likes = [];
+      const dislikes = [];
+
+      if (createMatchDto.isAMatch) {
+        likes.push(createMatchDto.userMatchReceiverUUID);
+      } else {
+        dislikes.push(createMatchDto.userMatchReceiverUUID);
+      }
+
+      return this.matchModel.create({
+        userUUID: createMatchDto.userMatchSenderUUID,
+        likes: likes,
+        dislikes: dislikes,
+      });
+    }
   }
 
   findAll() {
